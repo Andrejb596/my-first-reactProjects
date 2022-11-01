92babed8b19b5f1949e4dee8ad95cd065c76b93f
Andrejb596 <andrejb596@gmail.com>
2022-11-01 14:02:38 +0200

    Page_3 button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes as Switch, Route, NavLink } from "react-router-dom";
+import { HashRouter, Routes as Switch, Route, NavLink } from "react-router-dom";
 import './App.scss';
 import Home from "./pages/Home";
 import Page_1 from "./pages/Page_1";
@@ -12,9 +12,9 @@ import Page_6 from "./pages/Page_6";
 function App() {
 
   return (
-    <BrowserRouter>
+    <HashRouter>
       <nav className='app__nav'>
-        <NavLink to="my-first-reactProjects" className="navLink">Home</NavLink>
+        <NavLink to="Home" className="navLink">Home</NavLink>
         <NavLink to="Page_1" className="navLink">Counter</NavLink>
         <NavLink to="Page_2" className="navLink">Modal window</NavLink>
         <NavLink to="Page_3" className="navLink">Quiz</NavLink>
@@ -23,7 +23,7 @@ function App() {
         <NavLink to="Page_6" className="navLink">Tabs</NavLink>
       </nav>
       <Switch>
-        <Route path="my-first-reactProjects" element={<Home />} />
+        <Route path="Home" element={<Home />} />
         <Route path="Page_1" element={<Page_1 />} />
         <Route path="Page_2" element={<Page_2 />} />
         <Route path="Page_3" element={<Page_3 />} />
@@ -31,7 +31,7 @@ function App() {
         <Route path="Page_5" element={<Page_5 />} />
         <Route path="Page_6" element={<Page_6 />} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- src/pages/Page_3.jsx
+++ src/pages/Page_3.jsx
@@ -24,7 +24,7 @@ function Result({ correct }) {
     <div className="result">
       <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
       <h2>Ви зробили {correct} правильні відповіді з {questions.length}</h2>
-      <a href="Page_3">
+      <a href="/#/Page_3">
         <button>Спробувати знову</button>
       </a>
     </div>
