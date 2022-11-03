c8fd2616dd2c62bf7e11409de39dd721eed2fb3d
Andrejb596 <andrejb596@gmail.com>
2022-11-04 00:45:21 +0200

    page_8 cards good visible

--- src/pages/Page_8.jsx
+++ src/pages/Page_8.jsx
@@ -10,69 +10,91 @@ import iconSnow from '../components/Page_8/icon/iconSnow.png'
 import iconLetter from '../components/Page_8/icon/iconLetter.png'
 
 const initialArrayCards = [
-     { id: 1, img: iconBonfire },
-     { id: 2, img: iconUmbrella },
-     { id: 3, img: iconBulldozer },
-     { id: 4, img: iconMoose },
-     { id: 5, img: iconJacket },
-     { id: 6, img: iconSnow }
+  { id: 1, img: iconBonfire },
+  { id: 2, img: iconUmbrella },
+  { id: 3, img: iconBulldozer },
+  { id: 4, img: iconMoose },
+  { id: 5, img: iconJacket },
+  { id: 6, img: iconSnow }
 ]
 
 const pairOfArrayCards = [...initialArrayCards, ...initialArrayCards];
 
 export default function Page8() {
-     const [arrayCards, setArrayCards] = useState(pairOfArrayCards) // катрочки
-     const [openedCards, setOpenedCards] = useState([]);  // откритие карточки
-     const [matchad, setMatchad] = useState([]);
-     const [moves, setMoves] = useState(0); // шаги
+  const [arrayCards, setArrayCards] = useState(pairOfArrayCards) // катрочки
+  const [openedCards, setOpenedCards] = useState([]);  // откритие карточки
+  const [matched, setMatchad] = useState([]);
+  const [moves, setMoves] = useState(0); // шаги
 
-     const shuffle = (array) => {
-          let currentIndex = array.length,
-               tempararyValue,
-               randomIndex;
+  const shuffle = (array) => {
+    let currentIndex = array.length,
+      tempararyValue,
+      randomIndex;
 
-          while (currentIndex !== 0) {
-               randomIndex = Math.floor(Math.random() * currentIndex);
-               currentIndex -= 1;
+    while (currentIndex !== 0) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
 
-               tempararyValue = array[currentIndex];
-               array[currentIndex] = array[randomIndex];
-               array[randomIndex] = tempararyValue;
-          }
-          return array;
-     }
+      tempararyValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = tempararyValue;
+    }
+    return array;
+  }
 
-     useEffect(() => {
-          setArrayCards(() => shuffle(pairOfArrayCards))
-     }, [])
+  useEffect(() => {
+    setArrayCards(() => shuffle(pairOfArrayCards))
+  }, [])
 
+  const flipCard = (index) => () => {
+    setOpenedCards(openedCards => [...openedCards, index])
+    setMoves(prevMove => prevMove + 1)
+  }
 
-     return (
-          <div className="page8">
-               <p className='numberOfStrokes'>Зроблено ходів: {moves}</p>
-               <div className='cards'>
-                    {arrayCards.map((item, index) => {
-                         let isFlipt = false;
+  useEffect(() => {
+    if (openedCards < 2) return;
+    const firstMatched = arrayCards[openedCards[0]];
+    const secondMatched = arrayCards[openedCards[1]];
 
-                         if (openedCards.includes(index)) isFlipt = true;
-                         if (matchad.includes(item.id)) isFlipt = true;
+    if (secondMatched && firstMatched.id === secondMatched.id) {
+      setMatchad([...matched, firstMatched.id])
+    }
 
-                         return (
-                              <div key={index} className={`card ${isFlipt ? 'flipped' : ''}`}>
-                                   <div className='inner'>
-                                        <div className='front'>
-                                             <img src={item.img} width='100' alt='frontCard' />
-                                        </div>
-                                        <div className='back'>
-                                             <img src={iconLetter} width='100' alt='backCard' />
-                                        </div>
-                                   </div>
-                              </div>
-                         )
-                    })}
-               </div>
-          </div>
+    if (openedCards.length === 2) setTimeout(() => setOpenedCards([]), 1000);
+  }, [openedCards])
 
+  const handleGameRestart = () => {
+    setArrayCards(shuffle(pairOfArrayCards));
+    setOpenedCards([]);
+    setMatchad([]);
+    setMoves(0);
+  }
 
-     )
+  return (
+    <div className="page8">
+        <p className='numberOfStrokes'>Зроблено ходів: {moves}</p>
+        <div className='cards'>
+          {arrayCards.map((item, index) => {
+            let isFlipped = false;
+
+            if (openedCards.includes(index)) isFlipped = true;
+            if (matched.includes(item.id)) isFlipped = true;
+
+            return (
+              <div key={index} className={`card ${isFlipped ? 'flipped' : ''}`} onClick={flipCard(index)}>
+                <div className='inner'>
+                  <div className='front'>
+                    <img src={item.img} width='100' alt='frontCard' />
+                  </div>
+                  <div className='back'>
+                    <img src={iconLetter} width='100' alt='backCard' />
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+        <button className='buttonRestart' onClick={handleGameRestart}>Restart</button>
+      </div>
+  )
 }
